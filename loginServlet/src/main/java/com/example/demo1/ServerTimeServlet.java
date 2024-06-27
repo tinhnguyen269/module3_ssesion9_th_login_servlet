fa44d4f587ed0b9ed6e7e57d48f7655dce1a65f9
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-06-27 15:39:27 +0700

    firstcommit

---
+++ loginServlet/src/main/java/com/example/demo1/ServerTimeServlet.java
@@ -0,0 +1,20 @@
+package com.example.demo1;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", urlPatterns = "/ ")
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("");
+    }
+}
